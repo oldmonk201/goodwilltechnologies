cd299cdf14dcd9bb55eab06cfecde22c569e5bf2
Your Name <you@example.com>
2026-08-13 17:59:55 +0530

    changed wordpress.goodwilltechnologies.in to goodwilltechnologies.in

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://wordpress.goodwilltechnologies.in</homePageLink>
+		<homePageLink>https://goodwilltechnologies.in</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://wordpress.goodwilltechnologies.in/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://wordpress.goodwilltechnologies.in/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://wordpress.goodwilltechnologies.in/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://wordpress.goodwilltechnologies.in/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://wordpress.goodwilltechnologies.in/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://goodwilltechnologies.in/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://goodwilltechnologies.in/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://goodwilltechnologies.in/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://goodwilltechnologies.in/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://goodwilltechnologies.in/wp-json/" />
 			</apis>
 	</service>
 </rsd>
